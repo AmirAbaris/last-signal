3ba2725ac426beb304b44cd6f20a04bce20bb896
amir abaris <thisisamirabaris@gmail.com>
2026-02-08 00:27:50 +0330

    feat: add login page ui

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginCard from '@/components/auth/login-card'
+
+export default function LoginPage() {
+  return (
+    <div className="flex items-center justify-center min-h-dvh">
+      <LoginCard />
+    </div>
+  )
+}
