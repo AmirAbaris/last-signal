8e19855a67a62c107d6f2ef866e67d2dafd21e7a
amir abaris <thisisamirabaris@gmail.com>
2026-02-07 16:00:10 +0330

    init: nextjs init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactCompiler: true,
+  cacheComponents: true, // god help us on this feat!
 };
 
 export default nextConfig;
